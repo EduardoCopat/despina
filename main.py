446349307da5bce5d9b21238e906d7cf82872da5
EduardoCopat <eduardoc@zag.team>
2019-08-25 18:16:38 +1200

    Add event scripts, unformatted

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import xmltodict
-import json
+import simplejson as json
 import os
 from collections import OrderedDict
 
@@ -10,12 +10,33 @@ class NeptuneElement:
         self.id = None
         self.parent = None
         self.object = None
-        self.isContainer = False
         self.attributes = []
+        self.scripts = []
+        self.events_scripts = []
         
     def to_json(self):
         json_object = self.object.copy()
+
+        json_events = OrderedDict()
+        currentEvent = None
+        source_code = ""
+        for event_script in events_scripts:
+            event_name = event_script["EVENT"]
+            if(event_name != currentEvent):
+                currentEvent = event_name
+                source_code = ""
+            else:
+                if(event_script["TEXT"] != None):
+                    source_code = "\r\n" + source_code + event_script["TEXT"]
+            json_events[event_name] = source_code.encode()
+
+        json_object["Events_scripts"] = json_events
+        json_object["Attributes"] = self._attributes_to_json()
+        return json_object
+
+    def _attributes_to_json(self):
         json_attributes = OrderedDict()
+
         for attribute in attributes:
             group = attribute["GROUPING"]
             
@@ -25,9 +46,7 @@ class NeptuneElement:
             
             json_attributes[str(group)][attribute["ATTRIBUTE"]] = attribute["VALUE"]
 
-        json_object["Attributes"] = json_attributes
-        return json.dumps(json_object, sort_keys=True, indent=4)
-
+        return json_attributes
 
 
 def findInElements(elements, id):
@@ -48,13 +67,15 @@ objects = xml['asx:abap']['asx:values']['PROJECT']['IT_CONTENT']['_-NEPTUNE_-_OB
 
 attributes = xml['asx:abap']['asx:values']['PROJECT']['IT_ATTRIBUTES']['_-NEPTUNE_-_OBJ_AT']
 
+scripts = xml['asx:abap']['asx:values']['PROJECT']['IT_SCRIPT']['_-NEPTUNE_-_SCRIPT']
+
+events_scripts = xml['asx:abap']['asx:values']['PROJECT']['IT_EVENT_SCRIPT']['_-NEPTUNE_-_EVTSCR']
+
 for object in objects:
     element = NeptuneElement()
     element.id = object["FIELD_ID"]
     element.parent = object["FIELD_PARENT"]
     element.object = object
-    if(element.object["IS_CONTAINER"] == "X"):
-        element.isContainer == True
     elements.append(element)
 
 currentElement = None
@@ -63,10 +84,24 @@ for attribute in attributes:
         currentElement = findInElements(elements, attribute["FIELD_ID"])
     currentElement.attributes.append(attribute)
 
+currentElement = None
+for script in scripts:
+    if(currentElement == None or currentElement.id != script["FIELD_ID"]):
+        currentElement = findInElements(elements, script["FIELD_ID"])
+    currentElement.scripts.append(script)    
+
+currentElement = None
+for script in events_scripts:
+    if(currentElement == None or currentElement.id != script["FIELD_ID"]):
+        currentElement = findInElements(elements, script["FIELD_ID"])
+    currentElement.events_scripts.append(script)        
+
     
 def writeFile(file_name, element):
-    jsonFile = open(file_name + ".json","w+")
-    jsonFile.write(element.to_json())
+    jsonFile = open(file_name + ".json","w+", encoding="utf-8")
+    foo = element.to_json()
+    asd =  "\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\nosDetailOnline.setVisible(true);// Update DataModelData.Update(MasterLFA1,\"LIFNR\",modeloPageDetail.oData.LIFNR,modelDeltaLFA1.oData[0]);ModelData.Update(MasterADRC,\"ADDRNUMBER\",modeloPageDetail.oData.ADRNR,modelDeltaADRC.oData[0]);ModelData.Update(MasterLFM1,\"LIFNR\",modeloPageDetail.oData.LIFNR,modelDeltaLFM1.oData[0]);ModelData.Update(MasterBank,\"LIFNR\",modeloPageDetail.oData.LIFNR,modelDeltaBank.oData);// Set Customer DatasetDataVendor(modeloPageDetail.oData);// Save DatasetCacheMasterLFA1();setCacheMasterADRC();setCacheMasterLFM1();setCacheMasterBank();oApp.setBusy(false);".encode()
+    json.dump(foo, jsonFile, sort_keys=True, indent=4, ensure_ascii=False)
     jsonFile.close()
 
 def createFile(path, element):
